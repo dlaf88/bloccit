c4342ebde59ca88c1c02b39f03c27b6ca02d96d7
Diego Lafuente <dlaf88@gmail.com>
2015-03-13 19:13:48 +0000

    reinstaled figaro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  # For APIs, you may want to use :null_session instead. Some other change
   protect_from_forgery with: :exception
      before_action :configure_permitted_parameters, if: :devise_controller?
   
