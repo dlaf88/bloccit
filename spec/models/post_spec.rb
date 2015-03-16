99c617b915de19c7543eda9fef9f15a0b5711564
Diego Lafuente <dlaf88@gmail.com>
2015-03-16 22:59:32 +0000

    finished rspec validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,9 @@
    describe "vote methods" do
  
      before do
-       @post = Post.create(title: 'post title', body: 'post body')
+       user = User.create
+       topic = Topic.create
+       @post = Post.create(title: 'post title', body: 'post body must be pretty long')
        3.times { @post.votes.create(value: 1) }
        2.times { @post.votes.create(value: -1) }
      end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,14 +1,22 @@
+require 'rails_helper'
+
+
+     
+ 
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
+      before do
+       user = User.create
+       topic = Topic.create
+       @post = Post.create(title: 'post title', body: 'post body must be pretty long',user: user, topic: topic)
+       @post.votes.create(value: 5) 
+       
+     end
+      
       it "only allows -1 or 1 as values" do
-        
-        before do
-       @post = Post.create(title: 'post title', body: 'post body')
-       3.times { @post.votes.create(value: 1) }
-       2.times { @post.votes.create(value: -1) }       
-        end
- 
+        expect(@post.valid?).to eq(false)
         
       end
     end
