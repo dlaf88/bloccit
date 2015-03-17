100d58fa706706cb55e9abe1c75dbbf7ee382214
Diego Lafuente <dlaf88@gmail.com>
2015-03-17 02:52:24 +0000

    finished voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,8 @@ class Post < ActiveRecord::Base
   #validates :topic, presence: true
   #validates :user, presence: true
   
+  after_create :create_vote
+  
   def up_votes
      votes.where(value: 1).count
    end
@@ -31,7 +33,13 @@ class Post < ActiveRecord::Base
  
      update_attribute(:rank, new_rank)
    end
-  
+ 
+  private
+
+  # Who ever created a post, should automatically be set to "voting" it up.
+  def create_vote
+    self.user.votes.create(value: 1, post: self)
+  end  
   
   
   
