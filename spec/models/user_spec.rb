f8c187cd1b93a50a1a12bc99cecfeaa570e2cc8f
Diego Lafuente <dlaf88@gmail.com>
2015-03-17 16:09:24 +0000

    favorite assignment

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+ 
+ describe User do
+ 
+   include TestFactories
+ 
+   describe "#favorited(post)" do
+
+    before do 
+      @user = create(:user)
+      @post = create(:post)
+    end
+
+    it "returns `nil` if the user has not favorited the post" do
+      expect( @user.favorited(@post) ).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorited(@post) ).to eq(favorite)
+    end
+  end
+
+ end
